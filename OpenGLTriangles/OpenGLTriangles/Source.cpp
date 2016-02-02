4a4899826122c335184f389ad7e092a619da0c58
LauraLaureus <l4depende@gmail.com>
2016-02-02 09:54:09 +0000

    Finished

--- OpenGLTriangles/OpenGLTriangles/Source.cpp
+++ OpenGLTriangles/OpenGLTriangles/Source.cpp
@@ -8,3 +8,39 @@ void Init(){
 	glLoadIdentity();
 	glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 0.0f);
 }
+
+void Display(){
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.0f, 1.0f);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(0.0f, -1.0f);
+
+	glEnd();
+
+	glFlush();
+}
+
+int main(int argc, char *argv[]){
+	glutInit(&argc, argv);
+	glutInitWindowPosition(100, 200);
+	glutInitWindowSize(300, 200);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutCreateWindow("OpenGL Práctica 1_1 Laura");
+	Init();
+	glutDisplayFunc(Display);
+	glutMainLoop();
+	return 0;
+}
+
+
